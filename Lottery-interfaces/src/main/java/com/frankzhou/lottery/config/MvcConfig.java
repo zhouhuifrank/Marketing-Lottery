0c7e2a515d1a6b905d4b9941682dd6edfea26c50
frankzhou <2280610818@qq.com>
2023-02-08 18:35:18 +0800

    Redis新增

---
+++ Lottery-interfaces/src/main/java/com/frankzhou/lottery/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.frankzhou.lottery.config;
+
+import com.alibaba.nacos.shaded.org.checkerframework.checker.units.qual.C;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author This.FrankZhou
+ * @version 1.0
+ * @description 配置拦截器
+ * @date 2023-02-02
+ */
+@Configuration
+public class MvcConfig {
+}
