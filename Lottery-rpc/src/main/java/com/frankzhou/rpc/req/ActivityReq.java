876d0a4ef92189912054a4fdf1d8b424cb60acce
frankzhou <2280610818@qq.com>
2023-02-09 23:00:15 +0800

    Dubbo-RPC测试接口

--- Lottery-rpc/src/main/java/com/frankzhou/rpc/req/ActivityReq.java
+++ Lottery-rpc/src/main/java/com/frankzhou/rpc/req/ActivityReq.java
@@ -1,10 +1,20 @@
 package com.frankzhou.rpc.req;
 
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * @author This.FrankZhou
  * @version 1.0
  * @description
  * @date 2023-02-02
  */
-public class ActivityReq {
+@Data
+public class ActivityReq implements Serializable {
+    private static final long serialVersionUID = 1238913L;
+
+    private Long activityId;
+
+    private String userId;
 }
